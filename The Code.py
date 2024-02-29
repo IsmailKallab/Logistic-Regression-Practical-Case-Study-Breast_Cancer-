7e969b42f786926ceb74bf8d7999734f77cc186a
Ismail Kallab <137014927+IsmailKallab@users.noreply.github.com>
2024-02-29 21:50:40 +0300

    Update The Code.py

--- The Code.py
+++ The Code.py
@@ -10,7 +10,10 @@ y = dataset.iloc[:, -1].values
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state= 0)
-
+print(X_train)
+print(y_train)
+print(X_test)
+print(y_test)
 # Training the Logistic Regression model on the Training set
 
 from sklearn.linear_model import LogisticRegression
@@ -21,8 +24,13 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
-
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
 # Computing the accuracy with k-Fold cross-validation
-
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10 )
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
 
 
