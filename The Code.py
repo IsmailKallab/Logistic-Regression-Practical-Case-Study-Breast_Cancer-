a0c0e511af9bb231fa1bf4e719285eeb96d78d51
Ismail Kallab <137014927+IsmailKallab@users.noreply.github.com>
2024-02-29 20:08:05 +0300

    Update The Code.py

--- The Code.py
+++ The Code.py
@@ -1,1 +1,25 @@
-#
+# Logistic Regression
+# Importing the libraries
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('breast_cancer.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state= 0)
+
+# Training the Logistic Regression model on the Training set
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+
+# Computing the accuracy with k-Fold Cross Validation
